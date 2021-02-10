d3cc001980f5040a386edf03569da8e8953bc609
DiogoBarrosx <diogobarrosx@gmail.com>
2021-02-10 09:54:34 -0300

    tipos de ponto flutuante - curso c# microsoft learn

---
+++ modulo3/tipos-ponto-flutuante.cs
@@ -0,0 +1,14 @@
+/*
+
+Um ponto flutuante é um tipo de valor simples que representa números fracionários.
+
+Digitos de precisão é o número de valores que podem ser armazenados após o ponto decimal.
+
+
+*/
+
+
+Console.WriteLine("Floating point types:");
+Console.WriteLine($"float    : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+Console.WriteLine($"double   : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+Console.WriteLine($"decimal  : {decimal.MinValue} to {decimal.MaxValue} (with ~28-29 digits of precision)");
