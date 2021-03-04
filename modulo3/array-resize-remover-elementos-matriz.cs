9ba88c38144d37fae6f29637ef262d7b977bd852
DiogoBarrosx <diogobarrosx@gmail.com>
2021-03-04 10:26:52 -0300

    metodos para matrizes - array.resize - curso c# microsoft learn

---
+++ modulo3/array-resize-remover-elementos-matriz.cs
@@ -0,0 +1,39 @@
+/*
+
+Redimensionar a matriz para remover elementos.
+
+Observe que a última chamada de Array.Resize() não eliminou os dois primeiros elementos nulos.
+Em vez disso, ela removeu os três últimos elementos. Pois a matriz foi redefinida para ter 3 posições.
+
+*/
+
+string[] pallets = { "B14", "A11", "B12", "A13" };
+Console.WriteLine("");
+
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 3);
+Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
